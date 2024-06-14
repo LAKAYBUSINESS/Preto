77935053abb3ac0da9d8e76156e9ff1ef8572a89
Marina Lapsheva <marinacharles@Marinas-MacBook-Air.local>
2024-06-14 20:44:32 -0300

    Fix AppRegistry registration issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './App'; // Path adjusted to reflect App.js in root directory
+import App from './App'; // Adjust the path if necessary
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(main, () => App);
+AppRegistry.registerComponent(appName, () => App);
 
